06ba98ce056ab2e4ccf7482defb9999ba2621956
Pablo Sáez <pablosaez1995@gmail.com>
2023-02-21 17:55:31 -0300

    New "addEffect" function

--- packages/graphql-react-query/src/client.ts
+++ packages/graphql-react-query/src/client.ts
@@ -27,6 +27,16 @@ import type { ExecutionResult } from 'graphql';
 
 export type ExecutionResultWithData<Data> = Omit<ExecutionResult, 'data'> & { data: Data };
 
+export type EffectCallback<Result, Variables> = ({
+  operation,
+  result,
+  variables,
+}: {
+  operation: StringDocumentNode<Result, Variables>;
+  result: ExecutionResultWithData<Result>;
+  variables: Variables;
+}) => void;
+
 export function GraphQLReactQueryClient<
   Operations extends string = '',
   _OperationNames extends string = '',
@@ -41,6 +51,8 @@ export function GraphQLReactQueryClient<
   headers: Readonly<Record<string, unknown>>;
   fetchOptions?: Partial<RequestInit>;
 }) {
+  const effectsStore: Record<string, Set<EffectCallback<unknown, unknown>> | null> = {};
+
   async function fetcher<Result = unknown>({
     query,
     variables,
@@ -106,10 +118,41 @@ export function GraphQLReactQueryClient<
       });
     }
 
+    const effects = effectsStore[query];
+
+    if (effects) {
+      for (const effect of effects) {
+        try {
+          Promise.all([
+            effect({
+              operation: query as StringDocumentNode<unknown, unknown>,
+              result: { errors, data, extensions },
+              variables,
+            }),
+          ]).catch(() => null);
+        } catch (err) {}
+      }
+    }
+
     return {
       data,
       errors,
       extensions,
+    };
+  }
+
+  function addEffect<Result, Variables>(
+    operation: StringDocumentNode<Result, Variables>,
+    callback: EffectCallback<Result, Variables>,
+  ) {
+    const effects = (effectsStore[operation] ||= new Set());
+
+    effects.add(callback as EffectCallback<unknown, unknown>);
+
+    return function removeEffect() {
+      effects.delete(callback as EffectCallback<unknown, unknown>);
+
+      if (effects.size === 0) effectsStore[operation] = null;
     };
   }
 
@@ -407,5 +450,6 @@ export function GraphQLReactQueryClient<
     gql,
     invalidateOperations,
     resetOperations,
+    addEffect,
   };
 }
