d715d3ed492ab79d5424aae69a65f0303cb62b13
crStiv <cryptostiv7@gmail.com>
2024-11-22 23:16:25 +0100

    typo documents.ts
    It seems to be there is a missing comma in "releases(filter:$filter pagination:$pagination)"

--- examples/next/src/generated/documents.ts
+++ examples/next/src/generated/documents.ts
@@ -20,7 +20,7 @@ export const TestTwoDocument = 'query TestTwo{__typename now}' as unknown as Str
 export type TestTwoDocument = typeof TestTwoDocument;
 
 
-export const ReleasesTestDocument = 'query releasesTest($filter:ReleasesCursorFilterArgs!$pagination:ReleasesCursorConnectionArgs!){releases(filter:$filter pagination:$pagination){edges{node{id title artist{id name}}cursor}pageInfo{hasPreviousPage hasNextPage startCursor endCursor}}}' as unknown as StringDocumentNode<Types.ReleasesTestQuery,Types.ReleasesTestQueryVariables>;
+export const ReleasesTestDocument = 'query releasesTest($filter:ReleasesCursorFilterArgs!$pagination:ReleasesCursorConnectionArgs!){releases(filter:$filter, pagination:$pagination){edges{node{id title artist{id name}}cursor}pageInfo{hasPreviousPage hasNextPage startCursor endCursor}}}' as unknown as StringDocumentNode<Types.ReleasesTestQuery,Types.ReleasesTestQueryVariables>;
 export type ReleasesTestDocument = typeof ReleasesTestDocument;
 
 
@@ -30,6 +30,6 @@ export type CountDocument = typeof CountDocument;
 
 export type OperationNames = `Test` | `TestMutate` | `TestTwo` | `releasesTest` | `Count`;
 
-export type Operations = `query Test{__typename now}` | `mutation TestMutate($publicAddress:String!){generateAuthChallenge(publicAddress:$publicAddress)}` | `query TestTwo{__typename now}` | `query releasesTest($filter:ReleasesCursorFilterArgs!$pagination:ReleasesCursorConnectionArgs!){releases(filter:$filter pagination:$pagination){edges{node{id title artist{id name}}cursor}pageInfo{hasPreviousPage hasNextPage startCursor endCursor}}}` | `subscription Count{count(n:500)}`;
+export type Operations = `query Test{__typename now}` | `mutation TestMutate($publicAddress:String!){generateAuthChallenge(publicAddress:$publicAddress)}` | `query TestTwo{__typename now}` | `query releasesTest($filter:ReleasesCursorFilterArgs!$pagination:ReleasesCursorConnectionArgs!){releases(filter:$filter, pagination:$pagination){edges{node{id title artist{id name}}cursor}pageInfo{hasPreviousPage hasNextPage startCursor endCursor}}}` | `subscription Count{count(n:500)}`;
 
 
